5642aae9b80a529cba69a4aa52dd1d4fa7cb39d8
Rodion Glukhov <public.rodion@icloud.com>
2020-05-10 18:28:31 +0400

    Регистрация моделей в системе администраторов

--- source/polls/admin.py
+++ source/polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin, TabularInline
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+from polls.models import Poll, Question, User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    pass
+
+
+class QuestionInline(TabularInline):
+    model = Question
+
+
+@admin.register(Poll)
+class PollAdmin(ModelAdmin):
+    inlines = [QuestionInline]
