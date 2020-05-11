7f3c17fd222b1ea49f55352b43e74b547a413bb1
Rodion Glukhov <public.rodion@icloud.com>
2020-05-11 12:16:48 +0400

    Смена связей между моделями и обновление миграций

--- source/polls/models.py
+++ source/polls/models.py
@@ -7,42 +7,6 @@ class User(AbstractUser):
     """Пользователь системы."""
 
 
-def _limit_end_date_choices() -> dict:
-    return {'end_date__isnull': True}
-
-
-def _limit_is_active_choices() -> dict:
-    return {'is_active': True}
-
-
-class Question(Model):
-    class Type(TextChoices):
-        TEXT_ANSWER = 'TA', 'Ответ с текстом'
-        SINGLE_ANSWER = 'SA', 'Ответ с выбором одного варианта'
-        MULTIPLE_CHOICE = 'MC', 'Ответ с выбором нескольких вариантов'
-
-    text = CharField(max_length=254, verbose_name='Текст')
-    type = CharField(max_length=2, choices=Type.choices, verbose_name='Тип')
-    poll = ForeignKey('Poll', CASCADE,
-                      limit_choices_to=_limit_end_date_choices,
-                      verbose_name='Опрос')
-
-    class Meta:
-        verbose_name = 'Вопрос'
-        verbose_name_plural = 'Вопросы'
-
-
-class Answer(Model):
-    user = ForeignKey(User, CASCADE, limit_choices_to=_limit_is_active_choices,
-                      verbose_name='Пользователь')
-    question = ForeignKey(Question, CASCADE, verbose_name='Вопрос')
-    text = TextField(verbose_name='Текст отввета')
-
-    class Meta:
-        verbose_name = 'Ответ'
-        verbose_name_plural = 'Ответы'
-
-
 class Poll(Model):
     name = CharField(max_length=254, verbose_name='Название')
     start_date = DateField(verbose_name='Дата начала')
@@ -52,3 +16,86 @@ class Poll(Model):
     class Meta:
         verbose_name = 'Опрос'
         verbose_name_plural = 'Опросы'
+
+    def __str__(self):
+        return f'{self.name} ({self.start_date} - {self.end_date})'
+
+
+class Question(Model):
+    class TypeChoices(TextChoices):
+        TEXT_ANSWER = 'TA', 'Ответ с текстом'
+        SINGLE_ANSWER = 'SA', 'Ответ с выбором одного варианта'
+        MULTIPLE_CHOICE = 'MC', 'Ответ с выбором нескольких вариантов'
+
+    text = CharField(max_length=254, verbose_name='Текст')
+    type = CharField(max_length=2, choices=TypeChoices.choices,
+                     verbose_name='Тип')
+    poll = ForeignKey(Poll, CASCADE, verbose_name='Опрос')
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+    def __str__(self):
+        return f'{self.type}: {self.text}'
+
+
+class ExpectedAnswer(Model):
+    question = ForeignKey(Question, CASCADE, verbose_name='Тип вопроса')
+    content = TextField(verbose_name='Вариант ответа')
+
+    class Meta:
+        verbose_name = 'Ожидаемый вариант ответа'
+        verbose_name_plural = 'Ожидаемые варианты ответа'
+
+
+class AbstractChoiceOfAnswer(Model):
+    choice = ForeignKey(ExpectedAnswer, CASCADE, verbose_name='Вариант ответа')
+
+    class Meta:
+        abstract = True
+
+
+class SingleChoiceOfAnswer(AbstractChoiceOfAnswer):
+    class Meta:
+        verbose_name = 'Одиночный вариант ответа'
+        verbose_name_plural = 'Одиночные варианты ответа'
+
+
+class AbstractAnswer(Model):
+    user = ForeignKey(User, CASCADE, verbose_name='Пользователь')
+    question = ForeignKey(Question, CASCADE, verbose_name='Вопрос')
+
+    class Meta:
+        abstract = True
+
+
+class TextAnswer(AbstractAnswer):
+    text = TextField(verbose_name='Текст ответа')
+
+    class Meta:
+        verbose_name = 'Вариант ответа с текстом'
+        verbose_name_plural = 'Вариант ответов с текстом'
+
+
+class SingleChoiceAnswer(AbstractAnswer):
+    choice = ForeignKey(SingleChoiceOfAnswer, CASCADE, verbose_name='Ответ')
+
+    class Meta:
+        verbose_name = 'Вариант ответа с выбором одного варианта'
+        verbose_name_plural = 'Вариант ответов с выбором одного варианта'
+
+
+class AnswerWithMiltipleChoices(AbstractAnswer):
+    class Meta:
+        verbose_name = 'Вариант ответа с выбором нескольких вариантов'
+        verbose_name_plural = 'Вариант ответов с выбором нескольких вариантов'
+
+
+class MiltipleChoiceOfAnswer(AbstractChoiceOfAnswer):
+    answer = ForeignKey(AnswerWithMiltipleChoices, CASCADE,
+                        verbose_name='Ответ')
+
+    class Meta:
+        verbose_name = 'Множественный вариант ответа'
+        verbose_name_plural = 'Множественные варианты ответа'
