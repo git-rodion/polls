cdb6053c0630122707a1c859043bc0cda8f43e36
Rodion Glukhov <public.rodion@icloud.com>
2020-05-11 13:28:09 +0400

    Представление для возврата списка активных опросов

--- source/polls/views.py
+++ source/polls/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.utils.datetime_safe import datetime
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin
 
-# Create your views here.
+from polls.models import Poll
+from polls.serializers import PollSerializer
+
+
+class ActivePolls(ListModelMixin, GenericAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset.filter(start_date__gte=datetime.now().date(),
+                        end_date__lte=datetime.now().date())
+        return queryset
