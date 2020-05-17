fe9e7bde48b7da5c023f49ed155a912283e1a110
Rodion Glukhov <public.rodion@icloud.com>
2020-05-17 17:14:48 +0400

    Ещё немного проверок

--- source/polls/tests.py
+++ source/polls/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.utils.datetime_safe import date
 
-from polls.models import Poll
+from polls.models import AnswerOption, Poll, Question
 
 
 class PollTestCase(TestCase):
@@ -15,3 +15,35 @@ class PollTestCase(TestCase):
         poll = Poll.objects.get(name='Проверочный опрос')
         expected_result = f'Проверочный опрос ({self.today} - {self.today})'
         self.assertEqual(str(poll), expected_result)
+
+
+class QuestionTestCase(TestCase):
+    def setUp(self) -> None:
+        poll = Poll.objects.create(
+            name='Проверочный опрос', start_date=date.today(),
+            end_date=date.today(), description='Описание для опроса')
+        Question.objects.create(text='Что должно здесь быть?',
+                                type=Question.TypeChoices.TEXT_ANSWER,
+                                poll=poll)
+
+    def test_question_has_correct_str_view(self):
+        text = 'Что должно здесь быть?'
+        question = Question.objects.get(text=text)
+        self.assertEqual(str(question), f'TEXT_ANSWER: {text}')
+
+
+class AnswerOptionTestCase(TestCase):
+    def setUp(self) -> None:
+        poll = Poll.objects.create(name='Проверочный опрос',
+                                   start_date=date.today(),
+                                   end_date=date.today(),
+                                   description='Описание для опроса')
+        question = Question.objects.create(
+            text='Что должно здесь быть?',
+            type=Question.TypeChoices.TEXT_ANSWER, poll=poll)
+        AnswerOption.objects.create(question=question, content='Django')
+
+    def test_answer_option_has_correct_view(self):
+        content = 'Django'
+        answer_option = AnswerOption.objects.get(content=content)
+        self.assertEqual(str(answer_option), content)
